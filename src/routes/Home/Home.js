cd1340c71e5d8320c2085b7da604ebac1f8b1c1b
Isai Castro <isaicstr@gmail.com>
2022-10-25 16:38:06 -0700

    Added options table for user to choose league and year

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -4,6 +4,28 @@ const Home = () => {
   return (
     <div className="home-container">
       <h2>Find your favorite team's standings!</h2>
+      <div className="options">
+        <select name="league">
+          <option value="LaLiga">LaLiga</option>
+          <option value="Premier League">Premier League</option>
+          <option value="Champions League">Champions League</option>
+        </select>
+        <select name="year">
+          <option value="2010">2010</option>
+          <option value="2011">2011</option>
+          <option value="2012">2012</option>
+          <option value="2013">2013</option>
+          <option value="2014">2014</option>
+          <option value="2015">2015</option>
+          <option value="2016">2016</option>
+          <option value="2017">2017</option>
+          <option value="2018">2018</option>
+          <option value="2019">2019</option>
+          <option value="2020">2020</option>
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+        </select>
+      </div>
     </div>
   );
 };
