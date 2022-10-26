11b1392636e3fa7943b7de90abf27bb12eb6e901
Isai Castro <isaicstr@gmail.com>
2022-10-25 17:18:25 -0700

    Finished styling home page select options

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -5,12 +5,14 @@ const Home = () => {
     <div className="home-container">
       <h2>Find your favorite team's standings!</h2>
       <div className="options">
-        <select name="league">
+        <label htmlFor="season">Choose a League:</label>
+        <select name="league" id="league">
           <option value="LaLiga">LaLiga</option>
           <option value="Premier League">Premier League</option>
           <option value="Champions League">Champions League</option>
         </select>
-        <select name="year">
+        <label htmlFor="year">Choose a Year:</label>
+        <select name="year" id="year">
           <option value="2010">2010</option>
           <option value="2011">2011</option>
           <option value="2012">2012</option>
@@ -25,6 +27,8 @@ const Home = () => {
           <option value="2021">2021</option>
           <option value="2022">2022</option>
         </select>
+        <br />
+        <input type="submit" value="Submit" className="button-submit" />
       </div>
     </div>
   );
