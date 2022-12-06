ad30633b59000cf2bebe0a55fa75d21203c59484
Isai Castro <isaicstr@gmail.com>
2022-12-05 20:25:32 -0700

    Removed live scores component as it will no longer be needed. Replaced by next matches component.

--- src/routes/home/home.js
+++ src/routes/home/home.js
@@ -1,4 +1,3 @@
-import LiveScores from "../../Components/live-scores/live-scores.component";
 import News from "../../Components/news/news.component";
 
 import "./home.styles.scss";
@@ -6,7 +5,6 @@ import "./home.styles.scss";
 const Home = () => {
   return (
     <div className="home-container">
-      <LiveScores />
       <News />
     </div>
   );
