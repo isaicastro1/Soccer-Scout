b06a9f3c17e03779904204bbdc2a10fbd450e570
Isai Castro <isaicstr@gmail.com>
2022-12-10 17:00:58 -0700

    Finished sign in form component

--- src/Components/sign-in-form/sign-in-form.component.js
+++ src/Components/sign-in-form/sign-in-form.component.js
@@ -1,67 +1,91 @@
+import { LockOutlined, UserOutlined } from "@ant-design/icons";
 import { Button, Checkbox, Form, Input } from "antd";
-const SignInForm = () => {
+import { useState } from "react";
+
+import {
+  signInWithGoogle,
+  signInUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase";
+
+import "./sign-in-form.styles.scss";
+
+const App = () => {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setUserPassword] = useState("");
+
+  const setEmail = (event) => setUserEmail(event.target.value);
+  const setPassword = (event) => setUserPassword(event.target.value);
+
+  const signInUser = () =>
+    signInUserWithEmailAndPassword(userEmail, userPassword);
+
   return (
     <Form
-      name="basic"
-      labelCol={{
-        span: 8,
-      }}
-      wrapperCol={{
-        span: 16,
-      }}
+      name="normal_login"
+      className="login-form"
       initialValues={{
         remember: true,
       }}
-      autoComplete="off"
     >
-      <Form.Item
-        label="Username"
-        name="username"
-        rules={[
-          {
-            required: true,
-            message: "Please input your username!",
-          },
-        ]}
-      >
-        <Input />
+      <Form.Item className="sign-in-title">
+        <h3>SIGN IN</h3>
       </Form.Item>
 
       <Form.Item
-        label="Password"
+        name="email"
+        rules={[
+          {
+            required: true,
+            message: "Please input your email!",
+          },
+        ]}
+      >
+        <Input
+          prefix={<UserOutlined className="site-form-item-icon" />}
+          placeholder="Email"
+          onChange={setEmail}
+        />
+      </Form.Item>
+      <Form.Item
         name="password"
         rules={[
           {
             required: true,
-            message: "Please input your password!",
+            message: "Please input your Password!",
           },
         ]}
       >
-        <Input.Password />
+        <Input
+          prefix={<LockOutlined className="site-form-item-icon" />}
+          type="password"
+          placeholder="Password"
+          onChange={setPassword}
+        />
+      </Form.Item>
+      <Form.Item>
+        <Form.Item name="remember" valuePropName="checked" noStyle>
+          <Checkbox>Remember me</Checkbox>
+        </Form.Item>
       </Form.Item>
 
-      <Form.Item
-        name="remember"
-        valuePropName="checked"
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Checkbox>Remember me</Checkbox>
-      </Form.Item>
-
-      <Form.Item
-        wrapperCol={{
-          offset: 8,
-          span: 16,
-        }}
-      >
-        <Button type="primary" htmlType="submit">
-          Submit
+      <Form.Item className="sign-in-buttons">
+        <Button
+          type="primary"
+          htmlType="submit"
+          className="sign-in-button"
+          onClick={signInUser}
+        >
+          SIGN IN
+        </Button>
+        <Button
+          type="primary"
+          className="sign-with-google-button"
+          onClick={signInWithGoogle}
+        >
+          SIGN IN WITH GOOGLE
         </Button>
       </Form.Item>
     </Form>
   );
 };
-export default SignInForm;
+export default App;
