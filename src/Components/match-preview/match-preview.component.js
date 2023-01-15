33e14b42da71ca215fbce120abbb90cc3d1b5dbf
Isai Castro <isaicstr@gmail.com>
2023-01-14 20:05:08 -0700

    Added feature that shows trophy next to team that won the match

--- src/Components/match-preview/match-preview.component.js
+++ src/Components/match-preview/match-preview.component.js
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+
+import Trophy from "../../Assets/trophy.png";
+
 import "./match-preview.styles.scss";
 
 const MatchPreview = ({
@@ -11,11 +15,35 @@ const MatchPreview = ({
   homeGoals,
   awayGoals,
 }) => {
+  const [homeWon, setHomeWon] = useState(false);
+  const [matchEnded, setMatchEnded] = useState(false);
+
+  useEffect(() => {
+    const homeWin = () => {
+      if (homeGoals === null) return;
+      setHomeWon(homeGoals > awayGoals);
+      setMatchEnded(true);
+    };
+
+    homeWin();
+  }, [awayGoals, homeGoals]);
+
+  useEffect(() => {
+    "effect render";
+  }, [homeWon]);
+
   return (
     <div className="match-preview-container">
       <h3 className="match-game">{round}</h3>
       <div className="team-logos">
         <div className="team-logo-container">
+          {homeWon ? (
+            <div>
+              <img src={Trophy} style={{ height: "40px" }} alt="trophy" />
+            </div>
+          ) : (
+            <></>
+          )}
           <div className="team-name">
             <h5 className="team-one-name">{teamOneName}</h5>
           </div>
@@ -44,6 +72,13 @@ const MatchPreview = ({
           <div className="team-name">
             <h5 className="team-two-name">{teamTwoName}</h5>
           </div>
+          {!homeWon && matchEnded ? (
+            <div>
+              <img src={Trophy} style={{ height: "40px" }} alt="trophy" />
+            </div>
+          ) : (
+            <></>
+          )}
         </div>
       </div>
       <div className="see-more">see more</div>
