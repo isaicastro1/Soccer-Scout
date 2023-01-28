c6f91cb002302f28ff85f5045e5b5b76fe7d8906
Isai Castro <isaicstr@gmail.com>
2023-01-28 15:11:57 -0700

    Improveed code by using destructuring when mapping through team data array

--- src/Components/table/table.js
+++ src/Components/table/table.js
@@ -15,7 +15,7 @@ const Table = ({ leagueLogo, leagueName }) => {
         {allTeamData &&
           allTeamData.map((array) => {
             return (
-              <div className={`${leagueName}-table`} key={array[0].group}>
+              <div className={`${leagueName}-table`} key={array[1].group}>
                 <h2 key={array[0].group}>{array[0].group}</h2>
                 <table className="standings" key={array[0].team.name}>
                   <thead>
@@ -45,51 +45,50 @@ const Table = ({ leagueLogo, leagueName }) => {
                   </thead>
                   <tbody>
                     {array &&
-                      array.map((item) => {
-                        const id = item.team.id;
-                        const name = item.team.name;
-                        const logo = item.team.logo;
-                        const played = item.all.played;
-                        const wins = item.all.win;
-                        const draws = item.all.draw;
-                        const losses = item.all.lose;
-                        const goalsDiff = item.goalsDiff;
-                        const points = item.points;
-
-                        return (
-                          <tr className="team-row" key={id}>
-                            <td className="team-name">
-                              <img
-                                src={logo}
-                                style={{ height: 20, width: 20 }}
-                                alt={name}
-                              />
-                              <a
-                                href={`https://www.espn.com/soccer/team/_/id/${
-                                  teamId[name]
-                                }/${
-                                  name.includes(" ")
-                                    ? name.replace(" ", "-").toLowerCase()
-                                    : name.toLowerCase()
-                                }`}
-                                name=""
-                              >
-                                {name}
-                              </a>
-                            </td>
-                            <td className="right">{played}</td>
-                            <td className="right">{wins}</td>
-                            <td className="right">{draws}</td>
-                            <td className="right">{losses}</td>
-                            <td className="right">
-                              {goalsDiff.toString().includes("-")
-                                ? goalsDiff
-                                : `+${goalsDiff}`}
-                            </td>
-                            <td className="right">{points}</td>
-                          </tr>
-                        );
-                      })}
+                      array.map(
+                        ({
+                          team: { id, name, logo },
+                          all,
+                          goalsDiff,
+                          points,
+                        }) => {
+                          const {
+                            played,
+                            win: wins,
+                            draw: draws,
+                            lose: losses,
+                          } = all;
+                          return (
+                            <tr className="team-row" key={id}>
+                              <td className="team-name">
+                                <img src={logo} alt={name} />
+                                <a
+                                  href={`https://www.espn.com/soccer/team/_/id/${
+                                    teamId[name]
+                                  }/${
+                                    name.includes(" ")
+                                      ? name.replace(" ", "-").toLowerCase()
+                                      : name.toLowerCase()
+                                  }`}
+                                  name=""
+                                >
+                                  {name}
+                                </a>
+                              </td>
+                              <td className="right">{played}</td>
+                              <td className="right">{wins}</td>
+                              <td className="right">{draws}</td>
+                              <td className="right">{losses}</td>
+                              <td className="right">
+                                {goalsDiff.toString().includes("-")
+                                  ? goalsDiff
+                                  : `+${goalsDiff}`}
+                              </td>
+                              <td className="right">{points}</td>
+                            </tr>
+                          );
+                        }
+                      )}
                   </tbody>
                 </table>
               </div>
